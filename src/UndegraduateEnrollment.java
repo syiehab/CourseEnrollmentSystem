21d17acdef0bd22cf5105156d9def060c2df46ef
imrnzqwan <imrnzqwan@gmail.com>
2025-12-12 23:27:53 +0800

    Enrollment & UndergraduateEnrollment

--- src/UndegraduateEnrollment.java
+++ src/UndegraduateEnrollment.java
@@ -2,5 +2,30 @@ public class UndegraduateEnrollment extends Enrollment {
     private String programme;
     private int studyYear;
 
-    
+    public UndegraduateEnrollment(String enrollID, String courseCode, String studentName, String Semester, String programme, int studyYear){
+        super(enrollID, courseCode, studentName, Semester);
+        this.programme = programme;
+        this.studyYear = studyYear;
+    }
+
+    public String setProgramme(){
+        return programme;
+    }
+    public int setStudyYear(){
+        return studyYear;
+    }
+
+    public void getProgramme(String newProgramme){
+        this.programme = newProgramme;
+    }
+    public void getStudyYear(int newStudyYear){
+        this.studyYear = newStudyYear;
+    }
+
+    @Override
+    public String toString(){
+        super.toString();
+        return "Programme: " + programme +
+                "\nStudy Year: " + studyYear;
+    }
 }
